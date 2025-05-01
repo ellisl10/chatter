b1d23b74c0a1d1475b62717d82ee4945d4f91b91
dvo27 <danielnvo08@gmail.com>
2025-04-30 17:39:05 -0700

    implemented login page content and css

--- src/app/pages/login/Login.jsx
+++ src/app/pages/login/Login.jsx
@@ -1,8 +1,47 @@
+import './Login.css'
 
 export function Login() {
+
     return (
         <>
-            <h1>Login Page</h1>
+            <div id='main-container'>
+                <div id='login-section'>
+                    <div className="title-container">
+                        <h1 id='Chatter'>Chatter</h1>
+                        <h1>Log In</h1>
+                    </div>
+
+                    <div className="inputs">
+                        <div className="input">
+                            <input 
+                                type="text" 
+                                placeholder='Username' 
+                                />
+                        </div>
+                        <div className="input">
+                            <input 
+                                type="password" 
+                                placeholder='Password' 
+                                />
+                        </div>
+                    </div>
+
+
+                    <div className="submit-container">
+                        <div className="submit">
+                            <button>Sign In</button>
+                        </div>
+                    </div>
+                    <div className="forgot-password">Don't have an account yet? <span>Create an account!</span></div>
+                </div>
+
+
+
+                {/* Add picture here still and figure out responsive design for picture size */}
+                <div id="graphic-section">
+                    <h1>Picture</h1>
+                </div>
+            </div>
         </>
     )
 }
