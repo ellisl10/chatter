c183cb5fa4f9f7adad7ea748b71f763ef760783a
GreasySponge <112441845+GreasySponge@users.noreply.github.com>
2025-05-09 20:18:57 +0000

    Pathing

--- src/app/pages/chat/Chat.jsx
+++ src/app/pages/chat/Chat.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from 'react-router-dom';
 import './Chat.css';
-import NavigationBar from 'src/app/components/NavigationBar';
+import NavigationBar from 'src/app/components';
 
 const mockContacts = [
   { id: 1, name: 'John Doe', lastMessage: 'Last message' },
