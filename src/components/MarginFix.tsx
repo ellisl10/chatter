f817fc49efe9d687309d240dfa256898ce80f94e
GreasySponge <112441845+GreasySponge@users.noreply.github.com>
2025-05-09 19:29:16 -0700

    Create MarginFix.tsx

---
+++ src/components/MarginFix.tsx
@@ -0,0 +1,12 @@
+import {useEffect} from 'react';
+
+//Fixes the issue in App.css that is centering our pages.
+//Simply return this hook in your function on relevant pages.
+export default function useBodyClass(className: string) {
+    useEffect(() => {
+        document.body.classList.add(className);
+        return () => {
+            document.body.classList.remove(className);
+        };
+    }, [className]);
+}
