d578a99b4956e32d0ce81cb6b90ec91479dffe6d
GreasySponge <112441845+GreasySponge@users.noreply.github.com>
2025-05-10 01:46:27 +0000

    attempting de-lobotomy

--- src/app/pages/chat/Chat.jsx
+++ src/app/pages/chat/Chat.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from 'react-router-dom';
-import './Chat.css';
+import './Chat.module.css';
 import NavigationBar from '../../../components/NavigationBar';
 
 const mockContacts = [
