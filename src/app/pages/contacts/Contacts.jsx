d82438960518c02e5da940c0ae519c594e97e514
Edward Henriquez <ehenrque@ics.uci.edu>
2025-05-13 10:43:52 -0700

    Settings page implementation

--- src/app/pages/contacts/Contacts.jsx
+++ src/app/pages/contacts/Contacts.jsx
@@ -1,12 +1,47 @@
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+import NavigationBar from '../../../components/NavigationBar';
+import { Sidebar } from './Sidebar.jsx';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Contacts.css';
+=======
+>>>>>>> HEAD@{1}
 import React from 'react';
 import NavigationBar from "../../../components/NavigationBar";
+>>>>>>> 0437d66 (Settings Implementation)
 
-export function Contacts() {
-    return (
-        <>
+
+export const Contacts = () => {
+  return (
+    <>
         <NavigationBar />
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+        <div className="d-flex contacts">
+          <div className='sidebar'>
+            <Sidebar />
+          </div>
+          <div className="flex-grow-1 d-flex flex-column align-items-center justify-content-center bg-white">
+              <h3 className="fw-bold mb-4">Add Friends</h3>
+              <div className="d-flex align-items-center gap-3">
+                <div className="input-group rounded-pill border px-3">
+                    <input type="text" className="form-control border-0" placeholder="Enter a username" />
+                </div>
+                <button className="btn btn-primary rounded-pill px-6">Send Request</button>
+              </div>
+          </div>
+        </div>
+    </>
+  );
+}
+=======
+>>>>>>> HEAD@{1}
 
               <h1 style={{ textAlign: 'center', marginTop: 56 }}>Contacts Page</h1>
         </>
     )
 }
+>>>>>>> 0437d66 (Settings Implementation)
--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -4,6 +4,33 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './NavigationBar.css';
 
 export default function NavigationBar() {
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+    const location = useLocation();
+    const isActive = (path) => location.pathname === path;
+    return (
+        <nav className="navbar fixed-top navbar-expand-xl navbar-dark bg-dark">
+            <a className="navbar-brand" href="#">
+                <img src="src/assets/message-icon.png" className="chatter-logo" width="30" height="30" />
+                Chatter
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+                <div class="navbar-nav">
+                    <a class={`nav-item nav-link ${isActive('/') ? 'active' : ''}`} href="/">Home</a>
+                    <a class={`nav-item nav-link ${isActive('/login') ? 'active' : ''}`} href="/login">Login</a>
+                    <a class={`nav-item nav-link ${isActive('/register') ? 'active' : ''}`} href="/register">Register</a>
+                    <a class={`nav-item nav-link ${isActive('/chat') ? 'active' : ''}`} href="/chat">Chat</a>
+                    <a class={`nav-item nav-link ${isActive('/contacts') ? 'active' : ''}`} href="/contacts">Contacts</a>
+                </div>
+            </div>
+        </nav>
+    );
+=======
+>>>>>>> HEAD@{1}
   const location = useLocation();
   const isActive = (path) => location.pathname === path;
 
@@ -34,4 +61,8 @@ export default function NavigationBar() {
       </div>
     </nav>
   );
+<<<<<<< HEAD
+=======
+>>>>>>> 0437d66 (Settings Implementation)
+>>>>>>> HEAD@{1}
 }
