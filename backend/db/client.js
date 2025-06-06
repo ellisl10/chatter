254531613a025a7d2cceca366e7d7ddc745cce8c
GreasySponge <112441845+GreasySponge@users.noreply.github.com>
2025-06-06 22:00:48 +0000

    migrating to firebase

--- backend/db/client.js
+++ backend/db/client.js
@@ -1,3 +1,12 @@
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient()
-module.exports = prisma;
+import admin from 'firebase-admin';
+import serviceAccount from './serviceAccountKey.json' assert { type: "json" };
+
+// Prevent reinitializing if the server reloads
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+}
+
+const db = admin.firestore();
+export default db;
