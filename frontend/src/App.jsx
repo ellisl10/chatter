48c6313f94eb2d28fd8b02e2a6780f9d537cee42
Daniel Vo <114435634+dvo27@users.noreply.github.com>
2025-06-06 22:59:50 +0000

    added account creation to firebase db

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter } from 'react-router'
 import { Routes, Route } from 'react-router'
 import { Home } from './app/pages/home/Home.jsx'
-import { Login } from './app/pages/login/login.jsx'
+import { Login } from './app/pages/login/Login.jsx'
 import { Register } from './app/pages/registration/Register.jsx'
 import { Chat } from './app/pages/chat/Chat.jsx'
 import { ContactsPage } from './app/pages/contacts/ContactsPage.jsx'
