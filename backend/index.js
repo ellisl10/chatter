d9629bcddbb47b908d7108255671744f5e01f0fc
GreasySponge <112441845+GreasySponge@users.noreply.github.com>
2025-06-10 06:01:18 +0000

    images finally work

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ import cors from 'cors';
 import { onRequest } from 'firebase-functions/v2/https';
 import userRoutes from './routes/users.js';
 import messageRoutes from './routes/messages.js';
-import uploadRoute from './routes/upload.js';
+// import uploadRoute from './routes/upload.js';
 
 const app = express();
 app.use(cors());
@@ -11,7 +11,7 @@ app.use(express.json());
 
 app.use('/users', userRoutes);
 app.use('/messages', messageRoutes);
-app.use('/upload', uploadRoute);
+// app.use('/upload', uploadRoute);
 
 export const api = onRequest(
   {
